57012e6f99836566a17a6d31496f90865c293926
guilherme <chaves.guilherme@proton.me>
2024-01-12 16:57:27 -0300

    feat: set security configs, add user and role, update db seeding

--- 02_aula_security/_02-exercise_employee-auth/src/main/resources/import.sql
+++ 02_aula_security/_02-exercise_employee-auth/src/main/resources/import.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user (email, password) VALUES ('ana@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
+INSERT INTO tb_user (email, password) VALUES ('bob@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_department(name) VALUES ('Sales');
 INSERT INTO tb_department(name) VALUES ('Management');
 INSERT INTO tb_department(name) VALUES ('Training');
