4263b2ee4ac675102928a6209f62657e3a9af0a9
guilherme <chaves.guilherme@proton.me>
2024-01-12 19:23:58 -0300

    feat: add find all and insert events

--- src/main/java/com/devsuperior/demo/entities/Event.java
+++ src/main/java/com/devsuperior/demo/entities/Event.java
@@ -2,13 +2,7 @@ package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_event")
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,13 @@
+INSERT INTO tb_user (email, password) VALUES ('ana@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
+INSERT INTO tb_user (email, password) VALUES ('bob@gmail.com', '$2a$10$.b7kwNIK0.LgKORXat067um7QHYqk00j2W1auI805SWv4E.ckPgIS');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_city(name) VALUES ('São Paulo');
 INSERT INTO tb_city(name) VALUES ('Brasília');
 INSERT INTO tb_city(name) VALUES ('Fortaleza');
